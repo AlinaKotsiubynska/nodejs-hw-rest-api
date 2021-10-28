319ebf2fbb5a39c2fac5bb0d178ae99c34f9701f
Alina Kotsiubynska <alinakotsiubynska@gmail.com>
2021-10-28 17:14:49 +0300

    +add: sgMail setup

---
+++ src/utils/emailService.js
@@ -0,0 +1,9 @@
+const sgMail = require('@configs/email.configs')
+
+const msg = {
+  from: 'alinakotsiubynska@gmail.com', // Change to your verified sender
+}
+
+const emailService = async () => {
+
+}
